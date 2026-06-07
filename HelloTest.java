4a4da3e9c9ec7e8f55811b315ea1a1ab9cf59a02
Jasmeen Kaur <83873453+meenbajwa@users.noreply.github.com>
2026-06-06 22:49:23 -0400

    Fix test condition in HelloTest.java

--- HelloTest.java
+++ HelloTest.java
@@ -2,7 +2,8 @@ public class HelloTest {
     public static void main(String[] args) {
         String result = "Hello from Jenkins + Java!";
         
-        if (result.equals("wrong string")) {
+        // if (result.equals("wrong string")) {
+        if (result.equals("Hello from Jenkins + Java!")) {
             System.out.println("TEST PASSED");
         } else {
             System.out.println("TEST FAILED");
