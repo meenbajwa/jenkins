b726345d6b0e59e22947df3ca94bdc8627a922dc
Jasmeen Kaur <83873453+meenbajwa@users.noreply.github.com>
2026-06-06 22:45:23 -0400

    Change test string to 'wrong string' for failure

--- HelloTest.java
+++ HelloTest.java
@@ -2,7 +2,7 @@ public class HelloTest {
     public static void main(String[] args) {
         String result = "Hello from Jenkins + Java!";
         
-        if (result.equals("Hello from Jenkins + Java!")) {
+        if (result.equals("wrong string")) {
             System.out.println("TEST PASSED");
         } else {
             System.out.println("TEST FAILED");
